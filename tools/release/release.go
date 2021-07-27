c2153d30a232c863cf1f42aba540b747918fc1a0
Adam Janikowski <12255597+ajanikow@users.noreply.github.com>
2021-07-27 11:59:39 +0200

    [Feature] Darwin ARM64 (#297)

--- tools/release/release.go
+++ tools/release/release.go
@@ -47,6 +47,7 @@ var (
 
 	binaries = map[string]string{
 		"arangodb-darwin-amd64":      "darwin/amd64/arangodb",
+		"arangodb-darwin-arm64":      "darwin/arm64/arangodb",
 		"arangodb-linux-amd64":       "linux/amd64/arangodb",
 		"arangodb-linux-arm64":       "linux/arm64/arangodb",
 		"arangodb-windows-amd64.exe": "windows/amd64/arangodb.exe",
