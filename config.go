e1cee40817f9a23ea33b75cc46808f4fcec9282b
Nikita Vaniasin <nikita.vanyasin@gmail.com>
2022-10-13 13:58:45 +0300

    Ensure travis script is working

--- config.go
+++ config.go
@@ -1,3 +1,23 @@
+//
+// DISCLAIMER
+//
+// Copyright 2017-2022 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+
 package main
 
 import (
@@ -26,7 +46,6 @@ func loadCfgFromFile(cfgFilePath string) (*ini.File, error) {
 	return f, nil
 }
 
-//TODO: this line will be reverted
 // findFlagByName searches for a flag in provided flag sets
 func findFlagByName(n string, flagSets ...*pflag.FlagSet) *pflag.Flag {
 	for _, fs := range flagSets {
