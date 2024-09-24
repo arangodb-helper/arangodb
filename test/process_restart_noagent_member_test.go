3c7cd5d1886a4f765071f369bd733ced4908a73a
jwierzbo <jakub.wierzbowski@arangodb.com>
2024-09-24 10:47:07 +0200

    Fix test

--- test/process_restart_noagent_member_test.go
+++ test/process_restart_noagent_member_test.go
@@ -81,6 +81,10 @@ func TestProcessRestartNoAgentMember(t *testing.T) {
 	})
 
 	// TODO fix-me: GT-608
+	for _, m := range members {
+		defer m.Process.Close()
+	}
+	// TODO fix-me: GT-608
 	//SendIntrAndWait(t, members[10000].Process, members[6000].Process, members[7000].Process, members[8000].Process, members[9000].Process)
 }
 
@@ -129,8 +133,12 @@ func TestProcessMultipleRestartNoAgentMember(t *testing.T) {
 			verifySetupJson(t, members)
 			verifyEndpointSetup(t, members, "localhost")
 		})
-
 	}
+
+	for _, m := range members {
+		defer m.Process.Close()
+	}
+	//SendIntrAndWait(t, members[10000].Process, members[6000].Process, members[7000].Process, members[8000].Process, members[9000].Process)
 }
 
 func verifyEndpointSetup(t *testing.T, members map[int]MembersConfig, host string) {
