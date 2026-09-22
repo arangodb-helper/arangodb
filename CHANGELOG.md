fb916398b06770335384073631af032ce3d6dbfc
bluepal-prasanthi-moparthi <prasanthi.moparthi-external@arangodb.com>
2026-09-22 17:14:41 +0530

    fix(OAS-13128): support ES256 JWT authentication in Starter 0.19.x (#534)
    * fix(OAS-13128): support ES256 JWT authentication in Starter 0.19.x
    
    * fix: add missing license header to authentication tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # ArangoDB Starter Changelog
 
 ## [master](https://github.com/arangodb-helper/arangodb/tree/master) (N/A)
+- Sign JWTs with ES256 for PEM-encoded P-256 private keys in authentication commands and database, Agency, and coordinator clients; retain HS256 for shared secrets and reject invalid PEM keys.
 
 ## [v0.18.30](https://github.com/arangodb-helper/arangodb/tree/0.18.30) (2026-08-20)
 - Bump `github.com/moby/go-archive` to v0.3.3 for CVE fix (CVE-2026-17106)
--- service/service.go
+++ service/service.go
@@ -45,7 +45,6 @@ import (
 	driver "github.com/arangodb/go-driver"
 	"github.com/arangodb/go-driver/agency"
 	driver_http "github.com/arangodb/go-driver/http"
-	"github.com/arangodb/go-driver/jwt"
 
 	"github.com/arangodb-helper/arangodb/client"
 	"github.com/arangodb-helper/arangodb/pkg/definitions"
@@ -1000,8 +999,7 @@ func (s *Service) ChangeState(newState State) {
 // prepare a request to a database server (including authentication).
 func (s *Service) PrepareDatabaseServerRequestFunc() func(*http.Request) error {
 	return func(req *http.Request) error {
-		addJwtHeader(req, s.jwtSecret)
-		return nil
+		return addJwtHeader(req, s.jwtSecret)
 	}
 }
 
@@ -1052,7 +1050,7 @@ func (s *Service) CreateClient(endpoints []string, connectionType ConnectionType
 	if err != nil {
 		return nil, maskAny(err)
 	}
-	jwtBearer, err := jwt.CreateArangodJwtAuthorizationHeader(secret, "starter")
+	jwtBearer, err := CreateJwtAuthorizationHeader(secret, "starter")
 	if err != nil {
 		return nil, maskAny(err)
 	}
